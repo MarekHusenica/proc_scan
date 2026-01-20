7a7d103f741da76189a1135091f44e4153619d5f
MarekHusenica <marek.husenica@gmail.com>
2026-01-20 21:23:14 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,23 +2,30 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <errno.h>
+
+#include <string.h>
 #include <ctype.h>
-
-
-#define INITIAL_SIZE 64
 
 
 int main() {
 
     DIR *proc_dir = opendir("/proc");
-    struct dirent **entries = malloc(sizeof(struct dirent) * INITIAL_SIZE);
 
     while (proc_dir) {
         errno = 0;
         struct dirent *proc_entry;
         if ((proc_entry = readdir(proc_dir)) != NULL) {
-            if (isdigit)
 
+            int has_non_digit = 0;
+            for (size_t i = 0; i < strlen(proc_entry->d_name); i++) {
+                if (!isdigit(proc_entry->d_name[i])) {
+                    has_non_digit = 1;
+                    break;
+                }
+            }
+            if (has_non_digit) continue;
+
+            
         }
     }
 }
